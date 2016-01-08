e9369f8226b8e9122eec82ad189ec492647253cd
Keelan Massicotte <Keelan1013@gmail.com>
2016-01-08 16:39:40 -0500

    Added a very bad character controller

---
+++ Assets/Scripts/Butterman.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Butterman : MonoBehaviour {
+
+    [SerializeField] Rigidbody2D playerBody;
+    [SerializeField] float maxSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Jump")) {
+            this.playerBody.velocity = new Vector3(this.playerBody.velocity.x, this.playerBody.velocity.y + 10);
+        }
+
+	}
+
+    void FixedUpdate() {
+        this.playerBody.velocity = new Vector3(Input.GetAxis("Horizontal") * this.maxSpeed, this.playerBody.velocity.y);
+    }
+}
